6e772ffec6db2e37ac1da120abf23bab747fd0b7
Luca Scarabello <luca.scarabello@sed.ethz.ch>
2019-08-24 15:16:59 +0200

    BUG: revert wrong commit

--- apps/scrtdd/rtdd.cpp
+++ apps/scrtdd/rtdd.cpp
@@ -1431,7 +1431,7 @@ void RTDD::Profile::load(DatabaseQuery* query,
     hypodd = new HDD::HypoDD(ddbgc, ddcfg, pWorkingDir);
     hypodd->setWorkingDirCleanup(cleanupWorkingDir);
     hypodd->setUseCatalogDiskCache(cacheWaveforms);
-    hypodd->setUseSingleEvDiskCache(true); //cacheWaveforms && !incrementalCatalogFile.empty());
+    hypodd->setUseSingleEvDiskCache(cacheWaveforms && !incrementalCatalogFile.empty());
     loaded = true;
     lastUsage = Core::Time::GMT();
 
