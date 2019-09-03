7be47963d8b31bbdbee4cb4201e2ff0dfa8f5626
Luca Scarabello <luca.scarabello@sed.ethz.ch>
2019-09-03 10:41:53 +0200

    STY: updated license

--- apps/scrtdd/app.h
+++ apps/scrtdd/app.h
@@ -1,16 +1,19 @@
 /***************************************************************************
- *   Copyright (C) by ETHZ/SED, GNS New Zealand, GeoScience Australia      *
+ *   Copyright (C) by ETHZ/SED                                             *
  *                                                                         *
  *   You can redistribute and/or modify this program under the             *
- *   terms of the SeisComP Public License.                                 *
+ *   terms of the "SED Public License for Seiscomp Contributions"          *
+ *                                                                         *
+ *   You should have received a copy of the "SED Public License for        *
+ *   Seiscomp Contributions" with this. If not, you can find it at         *
+ *   http://www.seismo.ethz.ch/static/seiscomp_contrib/license.txt         *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   SeisComP Public License for more details.                             *
+ *   "SED Public License for Seiscomp Contributions" for more details      *
  *                                                                         *
- *   Developed by gempa GmbH                                               *
- *   Modified by Luca Scarabello <luca.scarabello@sed.ethz.ch>             *
+ *   Developed by Luca Scarabello <luca.scarabello@sed.ethz.ch>            *
  ***************************************************************************/
 
 
