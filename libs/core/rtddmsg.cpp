034aeaaa61592cd1a9eaebb51fb9006d6bae1247
Luca Scarabello <luca.scarabello@sed.ethz.ch>
2019-09-11 17:06:44 +0200

    STY: added missing license header

--- libs/core/rtddmsg.cpp
+++ libs/core/rtddmsg.cpp
@@ -1,3 +1,21 @@
+/***************************************************************************
+ *   Copyright (C) by ETHZ/SED                                             *
+ *                                                                         *
+ *   You can redistribute and/or modify this program under the             *
+ *   terms of the "SED Public License for Seiscomp Contributions"          *
+ *                                                                         *
+ *   You should have received a copy of the "SED Public License for        *
+ *   Seiscomp Contributions" with this. If not, you can find it at         *
+ *   http://www.seismo.ethz.ch/static/seiscomp_contrib/license.txt         *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   "SED Public License for Seiscomp Contributions" for more details      *
+ *                                                                         *
+ *   Developed by Luca Scarabello <luca.scarabello@sed.ethz.ch>            *
+ ***************************************************************************/
+ 
 #include "rtddmsg.h"
 
 namespace Seiscomp {
