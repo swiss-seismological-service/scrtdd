25e9dacb33a2321cfce0bc3efff9d73e2ff19830
Luca Scarabello <luca.scarabello@sed.ethz.ch>
2020-03-12 12:52:42 +0100

    STY: store cross correlation results in XCorrCache class
    This helps development of new features that require to use the xcorr
    results

--- apps/scrtdd/hypodd.h
+++ apps/scrtdd/hypodd.h
@@ -148,19 +148,40 @@ class HypoDD : public Core::BaseObject {
         static std::string relocationReport(const CatalogCPtr& relocatedEv);
 
     private:
+        class XCorrCache;
 
         std::string generateWorkingSubDir(const Catalog::Event& ev) const;
+
         CatalogPtr relocateEventSingleStep(const CatalogCPtr& evToRelocateCat, const std::string& workingDir,
                                 bool doXcorr, bool computeTheoreticalPhases,
                                 std::string hypoddCtrlFile, double minPhaseWeight,
                                 double minESdist, double maxESdist, double minEStoIEratio,
                                 int minDTperEvt, int maxDTperEvt, int minNumNeigh, int maxNumNeigh,
                                 int numEllipsoids, double maxEllipsoidSize);
+
         double computePickWeight(double uncertainty) const;
         double computePickWeight(const Catalog::Phase& phase) const;
         CatalogPtr filterPhasesAndSetWeights(const CatalogCPtr& catalog, const Catalog::Phase::Source& source,
                                    const std::vector<std::string>& PphaseToKeep,
                                    const std::vector<std::string>& SphaseToKeep) const;
+
+        CatalogPtr selectNeighbouringEvents(const CatalogCPtr& catalog,
+                                            const Catalog::Event& refEv,
+                                            const CatalogCPtr& refEvCatalog,
+                                            double minPhaseWeight = 0, double minESdis=0,
+                                            double maxESdis=-1, double minEStoIEratio=0,
+                                            int minDTperEvt=1, int maxDTperEvt=-1,
+                                            int minNumNeigh=1, int maxNumNeigh=-1,
+                                            int numEllipsoids=5, double maxEllipsoidSize=10,
+                                            bool keepUnmatched=false, int *numNeigh=nullptr) const;
+        std::map<unsigned,CatalogPtr> 
+        selectNeighbouringEventsCatalog(const CatalogCPtr& catalog, double minPhaseWeight,
+                                        double minESdis, double maxESdis, double minEStoIEratio,
+                                        int minDTperEvt, int maxDTperEvt,
+                                        int minNumNeigh, int maxNumNeigh,
+                                        int numEllipsoids, double maxEllipsoidSize,
+                                        bool keepUnmatched) const;
+
         void addMissingEventPhases(const CatalogCPtr& searchCatalog,
                                    const Catalog::Event& refEv,
                                    CatalogPtr& refEvCatalog);
@@ -178,85 +199,175 @@ class HypoDD : public Core::BaseObject {
                                              const std::string& phaseType,
                                              const Catalog::Event& refEv,
                                              const std::vector<HypoDD::PhasePeer>& peers,
-                                             double phaseVelocity);
+                                             double phaseVelocity); 
+
+        // Used to save xcorr results
+        class XCorrCache {
+
+        public:
+
+            struct XCorrCacheEntry {
+
+                double mean_coeff;
+                double mean_lag;
+                double min_lag;
+                double max_lag;
+                unsigned ccCount;
+
+                typedef struct {
+                    double coeff, lag, dtcc, weight, lowerUncertainty, upperUncertainty;
+                } PeerInfo;
+                std::map<unsigned,const PeerInfo> peers;
+
+                std::string peersStr; // debug
+
+                void update(const Catalog::Event& event, const Catalog::Phase& phase,
+                            double coeff, double lag, double dtcc, double weight)
+                {
+                    PeerInfo pi= {coeff, lag, dtcc, weight, phase.lowerUncertainty, phase.upperUncertainty};
+                    peers.insert( std::pair<unsigned,const PeerInfo>(event.id, pi) );
+                    peersStr   += std::string(event) + " ";
+                }
+
+                void computeStats()
+                {
+                    ccCount = peers.size();
+                    mean_coeff = 0;
+                    mean_lag   = 0;
+                    min_lag    = 0;
+                    max_lag    = 0;
+                    for ( auto& pair : peers )
+                    {
+                        const PeerInfo& data = pair.second;
+                        mean_coeff += std::abs(data.coeff);
+                        mean_lag   += data.lag;
+                        min_lag    += data.lag - data.lowerUncertainty;
+                        max_lag    += data.lag + data.upperUncertainty; 
+                    }
+                    mean_coeff /= ccCount;
+                    mean_lag   /= ccCount;
+                    min_lag    /= ccCount;
+                    max_lag    /= ccCount; 
+                }
+            };
+
+            XCorrCacheEntry& getForUpdate(unsigned evId, const std::string& stationId, const std::string& type)
+            {
+                std::string key = make_key(evId, stationId, type);
+                return resultsByPhase[key];
+            }
+
+            void computeStats()
+            {
+                for ( auto& pair : resultsByPhase )  pair.second.computeStats();
+            }
+
+            bool has(unsigned evId, const std::string& stationId, const std::string& type ) const
+            {
+                std::string key = make_key(evId, stationId, type);
+                return resultsByPhase.count(key) != 0;
+            }
+
+            const XCorrCacheEntry& get(unsigned evId, const std::string& stationId, const std::string& type ) const
+            {
+                std::string key = make_key(evId, stationId, type);
+                return resultsByPhase.at(key);
+            }
+
+            bool has(unsigned evId1, unsigned evId2, const std::string& stationId, const std::string& type ) const
+            {
+                return has(evId1, stationId, type) && (get(evId1, stationId, type).peers.count(evId2) != 0);
+            }
+
+            const XCorrCacheEntry::PeerInfo& get(unsigned evId1, unsigned evId2, const std::string& stationId, const std::string& type ) const
+            {
+                return get(evId1, stationId, type).peers.at(evId2);
+            } 
+
+        private:
+            static std::string make_key(unsigned evId, const std::string& stationId, const std::string& type )
+            {
+                return std::to_string(evId) + "." + stationId + "." + type; 
+            }
+
+            // cache of computed xcorr
+            std::map<std::string, XCorrCacheEntry> resultsByPhase;
+
+        };
+
+        struct PhaseXCorrCfg {
+            bool useDiskCache;
+            WfMngr::WfCache* cache;
+            bool allowSnrCheck;
+        };
+
+        XCorrCache buildXCorrCache(std::map<unsigned,CatalogPtr>& neighbourCats,
+                                   bool computeTheoreticalPhases);
+        XCorrCache buildXCorrCache(CatalogPtr& catalog, unsigned evToRelocateId,
+                                   bool computeTheoreticalPhases);
+        void buildXcorrDiffTTimePairs(CatalogPtr& catalog,
+                                      unsigned evToRelocateId,
+                                      bool computeTheoreticalPhases,
+                                      XCorrCache& xcorr);
+ 
+        bool xcorrPhases(const Catalog::Event& event1, const Catalog::Phase& phase1, PhaseXCorrCfg& phCfg1,
+                         const Catalog::Event& event2, const Catalog::Phase& phase2, PhaseXCorrCfg& phCfg2,
+                         double& coeffOut, double& lagOut, double& diffTimeOut, double& weightOut);
+        bool _xcorrPhases(const Catalog::Event& event1, const Catalog::Phase& phase1, PhaseXCorrCfg& phCfg1,
+                          const Catalog::Event& event2, const Catalog::Phase& phase2, PhaseXCorrCfg& phCfg2,
+                          double& coeffOut, double& lagOut, double& diffTimeOut, double& weightOut);
+        bool xcorr(const GenericRecordCPtr& tr1, const GenericRecordCPtr& tr2, double maxDelay,
+                   bool qualityCheck, double& delayOut, double& coeffOut) const;
+        Core::TimeWindow xcorrTimeWindowLong(const Catalog::Phase& phase) const;
+        Core::TimeWindow xcorrTimeWindowShort(const Catalog::Phase& phase) const;
+
+        void printCounters();
+
+        //
+        // External HypoDD program specific
+        //
         void runHypodd(const std::string& workingDir, const std::string& dtccFile,
                        const std::string& dtctFile, const std::string& eventFile,
                        const std::string& stationFile, const std::string& ctrlFile) const;
         void runPh2dt(const std::string& workingDir,
                       const std::string& stationFile,
                       const std::string& phaseFile) const;
-        CatalogPtr loadRelocatedCatalog(const CatalogCPtr& originalCatalog,
-                                        const std::string& ddrelocFile,
-                                        const std::string& ddresidualFile="") const;
-        CatalogPtr selectNeighbouringEvents(const CatalogCPtr& catalog,
-                                            const Catalog::Event& refEv,
-                                            const CatalogCPtr& refEvCatalog,
-                                            double minPhaseWeight = 0, double minESdis=0,
-                                            double maxESdis=-1, double minEStoIEratio=0,
-                                            int minDTperEvt=1, int maxDTperEvt=-1,
-                                            int minNumNeigh=1, int maxNumNeigh=-1,
-                                            int numEllipsoids=5, double maxEllipsoidSize=10,
-                                            bool keepUnmatched=false, int *numNeigh=nullptr) const;
-        std::map<unsigned,CatalogPtr> 
-        selectNeighbouringEventsCatalog(const CatalogCPtr& catalog, double minPhaseWeight,
-                                        double minESdis, double maxESdis, double minEStoIEratio,
-                                        int minDTperEvt, int maxDTperEvt,
-                                        int minNumNeigh, int maxNumNeigh,
-                                        int numEllipsoids, double maxEllipsoidSize,
-                                        bool keepUnmatched) const;
+
         void createStationDatFile(const CatalogCPtr& catalog, const std::string& staFileName) const;
         void createPhaseDatFile(const CatalogCPtr& catalog, const std::string& phaseFileName) const;
         void createEventDatFile(const CatalogCPtr& catalog, const std::string& eventFileName) const;
-        void createDtCtCatalog(const std::map<unsigned,CatalogPtr>& neighbourCats, const std::string& dtctFile) const;
-        void createDtCtSingleEvent(const CatalogCPtr& catalog,
-                                   unsigned evToRelocateId,
-                                   const std::string& dtctFile) const;
-        void buildAbsTTimePairs(const CatalogCPtr& catalog,
-                                 unsigned evToRelocateId,
-                                 std::ofstream& outStream) const;
+
+        void createDtCt(std::map<unsigned,CatalogPtr>& neighbourCats, const std::string& dtctFile) const;
+        void createDtCt(CatalogPtr& catalog, unsigned evToRelocateId, const std::string& dtctFile) const;
+        void writeAbsTTimePairs(const CatalogCPtr& catalog, unsigned evToRelocateId, std::ofstream& outStream) const;
+
         std::set<unsigned> createDtCcPh2dt(const CatalogCPtr& catalog,
                                            const std::string& dtctFile,
                                            const std::string& dtccFile);
-        void createDtCcCatalog(std::map<unsigned,CatalogPtr>& neighbourCats,
-                               const std::string& dtccFile, bool computeTheoreticalPhases);
-        void createDtCcSingleEvent(CatalogPtr& catalog, unsigned evToRelocateId,
-                                   const std::string& dtccFile, bool computeTheoreticalPhases);
-        void buildXcorrDiffTTimePairs(CatalogPtr& catalog,
+        void createDtCc(std::map<unsigned,CatalogPtr>& neighbourCats,
+                        const std::string& dtccFile, const XCorrCache& xcorr);
+        void createDtCc(CatalogPtr& catalog, unsigned evToRelocateId,
+                         const std::string& dtccFile, const XCorrCache& xcorr);
+        void writeXcorrDiffTTimePairs(CatalogPtr& catalog,
                                       unsigned evToRelocateId,
-                                      bool computeTheoreticalPhases,
-                                      std::ofstream* outStream);
-                                      
-        struct XcorrPhasesCfg {
-            bool useDiskCache;
-            WfMngr::WfCache* cache;
-            bool allowSnrCheck;
-        };  
-        
-        bool xcorrPhases(const Catalog::Event& event1, const Catalog::Phase& phase1, XcorrPhasesCfg& phCfg1,
-                         const Catalog::Event& event2, const Catalog::Phase& phase2, XcorrPhasesCfg& phCfg2,
-                         double& coeffOut, double& lagOut, double& diffTimeOut, double& weightOut);
-        bool _xcorrPhases(const Catalog::Event& event1, const Catalog::Phase& phase1, XcorrPhasesCfg& phCfg1,
-                          const Catalog::Event& event2, const Catalog::Phase& phase2, XcorrPhasesCfg& phCfg2,
-                          double& coeffOut, double& lagOut, double& diffTimeOut, double& weightOut);
-        bool xcorr(const GenericRecordCPtr& tr1, const GenericRecordCPtr& tr2, double maxDelay,
-                          bool qualityCheck, double& delayOut, double& coeffOut) const;                          
-        Core::TimeWindow xcorrTimeWindowLong(const Catalog::Phase& phase) const;
-        Core::TimeWindow xcorrTimeWindowShort(const Catalog::Phase& phase) const;
-                                     
-        void printCounters();
+                                      const XCorrCache& xcorr,
+                                      std::ofstream& outStream);
 
+        CatalogPtr loadRelocatedCatalog(const CatalogCPtr& originalCatalog,
+                                        const std::string& ddrelocFile,
+                                        const std::string& ddresidualFile="") const;
     private:
         bool _workingDirCleanup = true;
         std::string _workingDir;
         std::string _cacheDir;
         std::string _wfDebugDir;
-        
+
         CatalogCPtr _srcCat;
         CatalogCPtr _ddbgc;
 
         const Config _cfg;
-                
-        WfMngrPtr  _wf;        
+
+        WfMngrPtr  _wf;
         bool _useCatalogDiskCache = false;
         WfMngr::WfCache _wfCache;
 
