092bb5ee4ac77507d0c4802cfd3dff1f86cf18b3
Luca Scarabello <luca.scarabello@sed.ethz.ch>
2020-11-05 14:04:13 +0100

    BLD: bump version to 1.2.0

--- apps/scrtdd/rtdd.h
+++ apps/scrtdd/rtdd.h
@@ -60,7 +60,7 @@ public:
   };
   DEFINE_SMARTPOINTER(Region);
 
-  virtual const char *version() { return "1.1.2+"; }
+  virtual const char *version() { return "1.2.0"; }
 
 protected:
   void createCommandLineDescription();
