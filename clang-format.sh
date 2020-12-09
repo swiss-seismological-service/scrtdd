3a51acd5af4bc5fa8980f3a1d5e259034430f35e
Luca Scarabello <luca.scarabello@sed.ethz.ch>
2020-12-09 16:44:01 +0100

    MAINT: updated clang-format.sh to include ipp files

--- clang-format.sh
+++ clang-format.sh
@@ -1,1 +1,1 @@
-clang-format -style=file -i apps/scrtdd/*.{cpp,h} apps/scrtdd/hdd/*.{cpp,h} libs/core/*.{cpp,h} plugins/locator/rtddloc/*.{cpp,h}
+clang-format -style=file -i apps/scrtdd/*.{cpp,h} apps/scrtdd/hdd/*.{cpp,h,ipp} libs/core/*.{cpp,h} plugins/locator/rtddloc/*.{cpp,h}
