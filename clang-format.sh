9ed644e1277a91354e70383d08d5f0d57f75ff88
Luca Scarabello <luca.scarabello@gmail.com>
2023-09-23 15:28:38 +0200

    STY: clang format

--- clang-format.sh
+++ clang-format.sh
@@ -1,1 +1,1 @@
-clang-format -style=file -i apps/scrtdd/*.{cpp,h} libs/hdd/*.{cpp,h} libs/hddsc/*.{cpp,h}
+clang-format -style=file -i apps/scrtdd/*.{cpp,h} libs/hdd/*.{cpp,h} libs/hdd/ttt/*.{cpp,h} libs/hdd/nll/*.{cpp,h} libs/hddsc/*.{cpp,h}
