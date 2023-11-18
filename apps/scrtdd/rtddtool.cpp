658071d739bc2b972ba18fddf643342bee71ef32
Luca Scarabello <luca.scarabello@gmail.com>
2023-11-18 16:00:48 +0100

    ENH/STY: rename xcorr.detectMissingPhases* options
    xcorr.detectMissingPhasesAutoOrigin -> xcorr.singleEventPhaseDetect.automaticOrigin
    xcorr.detectMissingPhasesManualOrigin -> xcorr.singleEventPhaseDetect.manualOrigin

--- apps/scrtdd/rtddtool.cpp
+++ apps/scrtdd/rtddtool.cpp
@@ -755,7 +755,7 @@ bool RTDD::validateParameters()
     try
     {
       prof->detectMissingPhasesAuto =
-          configGetBool(prefix + "detectMissingPhasesAutoOrigin");
+          configGetBool(prefix + "singleEventPhaseDetect.automaticOrigin");
     }
     catch (...)
     {
@@ -764,7 +764,7 @@ bool RTDD::validateParameters()
     try
     {
       prof->detectMissingPhasesManual =
-          configGetBool(prefix + "detectMissingPhasesManualOrigin");
+          configGetBool(prefix + "singleEventPhaseDetect.manualOrigin");
     }
     catch (...)
     {
